2165625e1a9e5ce5615616f4aa47e70471b64288
SashaShulha <sashamiller666@gmail.com>
2024-07-09 22:16:38 +0300

    deleted param userNewUrlParser for connect to db in api

--- api/helpers/db.js
+++ api/helpers/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { db } = require('../configuration/index');
 
 module.exports.connectDb = () => {
-  mongoose.connect(db, { userNewUrlParser: true });
+  mongoose.connect(db);
 
   return mongoose.connection;
 };
